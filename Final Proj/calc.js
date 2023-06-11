b61a2343003dd9f02bc0c5dc8d7c55de86b1b340
JD Biñegas <john.binegas@lpunetwork.edu.ph>
2023-06-11 19:02:57 +0800

    FINAL

---
+++ Final Proj/calc.js
@@ -0,0 +1,33 @@
+let display = document.getElementById("display");
+let memory = document.getElementById("memory");
+
+function appendValue(value) {
+  display.value += value;
+}
+
+function clearEntry() {
+  display.value = "";
+}
+
+function clearDisplay() {
+  display.value = "";
+  memory.innerHTML = "";
+}
+
+function calculate() {
+  let expression = display.value;
+  let result;
+
+  try {
+    result = eval(expression);
+    memory.innerHTML += expression + " = " + result + "<br>";
+  } catch (error) {
+    alert("Erro ao calcular. Verifique a expressão.");
+    result = "";
+  }
+
+  display.value = result;
+}
+function openLink(url) {
+  window.open(url, "_blank");
+}
